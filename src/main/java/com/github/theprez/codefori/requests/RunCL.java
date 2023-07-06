86522181294a1bcd6fda74cae5e99a143dd3e56a
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2023-07-06 15:48:24 -0500

    Remove extraneous SQL0443s from CL commands

--- src/main/java/com/github/theprez/codefori/requests/RunCL.java
+++ src/main/java/com/github/theprez/codefori/requests/RunCL.java
@@ -36,7 +36,7 @@ public class RunCL extends BlockRetrievableRequest {
         final String cmd = getRequestField("cmd").getAsString();
         final AS400JDBCConnection jdbcConn = getSystemConnection().getJdbcConnection();
         Statement jobLogPosStmt = jdbcConn.createStatement();
-        ResultSet posRs = jobLogPosStmt.executeQuery("SELECT COUNT(*)  FROM TABLE(QSYS2.JOBLOG_INFO('*')) A ");
+        ResultSet posRs = jobLogPosStmt.executeQuery("SELECT COUNT(*)  FROM TABLE(QSYS2.JOBLOG_INFO('*')) A WHERE A.FROM_MODULE NOT IN ('QSQCALLSP') OR A.FROM_MODULE IS NULL ");
         posRs.next();
         int pos = posRs.getInt(1);
         posRs.close();
@@ -52,7 +52,7 @@ public class RunCL extends BlockRetrievableRequest {
         }
 
         Statement resultsStmt = jdbcConn.createStatement();
-        m_rs = resultsStmt.executeQuery(" SELECT MESSAGE_ID || ' [' || SUBSTR(MESSAGE_TYPE,0,5) || ']: ' || MESSAGE_TEXT as summary, SEVERITY, MESSAGE_SECOND_LEVEL_TEXT, MESSAGE_ID,MESSAGE_TYPE,MESSAGE_SUBTYPE,SEVERITY,MESSAGE_TIMESTAMP,FROM_LIBRARY,FROM_PROGRAM,FROM_MODULE,FROM_PROCEDURE,FROM_INSTRUCTION,TO_LIBRARY,TO_PROGRAM,TO_MODULE,TO_PROCEDURE,TO_INSTRUCTION,FROM_USER,MESSAGE_LIBRARY,MESSAGE_FILE,MESSAGE_TOKEN_LENGTH,MESSAGE_TOKENS,MESSAGE_TEXT,MESSAGE_SECOND_LEVEL_TEXT,MESSAGE_KEY,QUALIFIED_JOB_NAME  FROM TABLE(QSYS2.JOBLOG_INFO('*')) A limit 99999 offset " + pos);
+        m_rs = resultsStmt.executeQuery(" SELECT MESSAGE_ID || ' [' || SUBSTR(MESSAGE_TYPE,0,5) || ']: ' || MESSAGE_TEXT as summary, SEVERITY, MESSAGE_SECOND_LEVEL_TEXT, MESSAGE_ID,MESSAGE_TYPE,MESSAGE_SUBTYPE,SEVERITY,MESSAGE_TIMESTAMP,FROM_LIBRARY,FROM_PROGRAM,FROM_MODULE,FROM_PROCEDURE,FROM_INSTRUCTION,TO_LIBRARY,TO_PROGRAM,TO_MODULE,TO_PROCEDURE,TO_INSTRUCTION,FROM_USER,MESSAGE_LIBRARY,MESSAGE_FILE,MESSAGE_TOKEN_LENGTH,MESSAGE_TOKENS,MESSAGE_TEXT,MESSAGE_SECOND_LEVEL_TEXT,MESSAGE_KEY,QUALIFIED_JOB_NAME  FROM TABLE(QSYS2.JOBLOG_INFO('*')) A WHERE A.FROM_MODULE NOT IN ('QSQCALLSP') OR A.FROM_MODULE IS NULL limit 99999 offset " + pos);
         addReplyData("data", super.getNextDataBlock(Integer.MAX_VALUE));
         addReplyData("is_done", true);
         if(null != callException) {
