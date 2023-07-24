4cdad224c700cf0b4e103a4ad741e7441a952a0a
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2023-07-24 14:26:55 -0500

    Ability to connect multiple ways (#7)

--- src/main/java/com/github/theprez/codefori/SystemConnection.java
+++ src/main/java/com/github/theprez/codefori/SystemConnection.java
@@ -1,80 +1,105 @@
 package com.github.theprez.codefori;
 
-import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
-
-import com.github.theprez.jcmdutils.AppLogger;
-import com.github.theprez.jcmdutils.ConsoleQuestionAsker;
-import com.ibm.as400.access.AS400;
-import com.ibm.as400.access.AS400JDBCConnection;
-import com.ibm.as400.access.AS400JDBCDataSource;
+import com.ibm.as400.access.AS400JDBCDriver;
 
 public class SystemConnection {
     public static class ConnectionOptions {
 
-        private String m_jdbcProps;
+        public enum ConnectionMethod {
+            TCP,
+            CLI;
+
+            public String getConnectionString() {
+                if (!isRunningOnIBMi()) {
+                    String hostname = "mysystem";
+                    String username = "user";
+                    String password = "pw";
+                    return String.format("jdbc:as400:%s;user=%s;password=%s", hostname, username, password);
+                }
+                switch (this) {
+                    case CLI:
+                        return Boolean.getBoolean("jdbc.db2.restricted.local.connection.only") ? "jdbc:default:connection":"jdbc:db2:localhost";
+                    default:
+                        return "jdbc:as400:localhost";
+                }
+            }
+        }
+
+        private String m_jdbcProps = "";
+        private ConnectionMethod m_connectionMethod = ConnectionMethod.CLI;
 
         String getJdbcProperties() {
             return m_jdbcProps;
+        }
+
+        public void setConnectionMethod(ConnectionMethod _m) {
+            m_connectionMethod = _m;
         }
 
         public void setJdbcProperties(final String _props) {
             m_jdbcProps = _props;
         }
 
-    }
-
-    private final AS400 m_as400;
-
-    private AS400JDBCConnection m_conn;
-
-    public SystemConnection() throws IOException {
-        if (System.getProperty("os.name", "").equalsIgnoreCase("OS/400")) {
-            m_as400 = new AS400("localhost", "*CURRENT", "*CURRENT");
-        } else {
-            final AppLogger logger = AppLogger.getSingleton(false);
-            final ConsoleQuestionAsker asker = new ConsoleQuestionAsker();
-            final String host = asker.askNonEmptyStringQuestion(logger, null, "Enter system name: ");
-            final String uid = asker.askNonEmptyStringQuestion(logger, null, "Enter user id: ");
-            final String pw = asker.askUserForPwd("Password: ");
-            m_as400 = new AS400(host, uid, pw);
+        public ConnectionMethod getConnectionMethod() {
+            return m_connectionMethod;
         }
+
     }
 
-    public AS400 getAs400() {
-        return m_as400;
+    private Connection m_conn;
+    private ConnectionOptions m_connectionOptions = null;
+
+    public static boolean isRunningOnIBMi() {
+        return System.getProperty("os.name", "").contains("400");
     }
 
-    public synchronized AS400JDBCConnection getJdbcConnection() throws SQLException {
+    public synchronized Connection getJdbcConnection() throws SQLException {
         if (null != m_conn && !m_conn.isClosed()) {
             return m_conn;
         }
         if (Boolean.getBoolean("codeserver.jdbc.autoconnect")) {
-            final AS400JDBCDataSource ds = new AS400JDBCDataSource(m_as400);
-            return m_conn = (AS400JDBCConnection) ds.getConnection();
+            return reconnect(m_connectionOptions);
         }
         throw new SQLException("Not connected");
     }
 
     public String getJdbcJobName() throws SQLException {
-        return makePrettyJobName(getJdbcConnection().getServerJobIdentifier());
+        try {
+            Connection c = getJdbcConnection();
+            Class<Connection> connectionClass = (Class<Connection>) c.getClass();
+            boolean isNativeDriver = connectionClass.getSimpleName().equalsIgnoreCase("DB2Connection");
+            String methodName = isNativeDriver ? "getServerJobName" : "getServerJobIdentifier";
+            String driverSuppliedName = c.getClass()
+                    .getMethod(methodName).invoke(c).toString();
+            return isNativeDriver ? driverSuppliedName : makePrettyJobNameFromJt400Name(driverSuppliedName);
+        } catch (Exception e) {
+            Tracer.err(e);
+            return "??????/??????/??????";
+        }
     }
 
-    private String makePrettyJobName(final String _jobString) {
+    private String makePrettyJobNameFromJt400Name(final String _jobString) {
         final String name = _jobString.substring(0, 10).trim();
         final String user = _jobString.substring(10, 20).trim();
         final String number = _jobString.substring(20).trim();
         return String.format("%s/%s/%s", number, user, name);
     }
 
-    public synchronized AS400JDBCConnection reconnect(final ConnectionOptions _opts) throws SQLException {
+    public synchronized Connection reconnect(final ConnectionOptions _opts) throws SQLException {
         if (null != m_conn) {
-            final AS400JDBCConnection cpy = m_conn;
+            final Connection cpy = m_conn;
             m_conn = null;
             cpy.close();
         }
-        final AS400JDBCDataSource ds = new AS400JDBCDataSource(m_as400);
-        ds.setProperties(_opts.getJdbcProperties());
-        return m_conn = (AS400JDBCConnection) ds.getConnection();
+        try {
+            DriverManager.registerDriver(new AS400JDBCDriver());
+            m_connectionOptions = _opts;
+            return m_conn = DriverManager.getConnection(_opts.getConnectionMethod().getConnectionString() + ";" + _opts.getJdbcProperties());
+        } catch (Exception e) {
+            throw new SQLException(e);
+        }
     }
 }
