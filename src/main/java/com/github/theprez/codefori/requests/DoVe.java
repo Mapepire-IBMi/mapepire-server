d13b59a2e6d3c0a9afb6378006db6b33a89e2355
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2023-09-22 22:09:10 -0500

    Update DoVe.java

--- src/main/java/com/github/theprez/codefori/requests/DoVe.java
+++ src/main/java/com/github/theprez/codefori/requests/DoVe.java
@@ -12,11 +12,10 @@ import javax.naming.spi.DirStateFactory.Result;
 import com.github.theprez.codefori.DataStreamProcessor;
 import com.github.theprez.codefori.SystemConnection;
 import com.github.theprez.codefori.Tracer;
+import com.github.theprez.jcmdutils.StringUtils;
 import com.google.gson.JsonObject;
 
 public class DoVe extends BlockRetrievableRequest {
-
-    private Statement m_stmt = null;
 
     public DoVe(final DataStreamProcessor _io, final SystemConnection m_conn, final JsonObject _reqObj) {
         super(_io, m_conn, _reqObj);
@@ -32,14 +31,14 @@ public class DoVe extends BlockRetrievableRequest {
         final String sql = getRequestField("sql").getAsString();
         final Connection jdbcConn = getSystemConnection().getJdbcConnection();
         byte[] id = new byte[0];
-        try(Statement s = jdbcConn.createStatement()) {
+        try (Statement s = jdbcConn.createStatement()) {
             s.execute("CALL QSYS2.QCMDEXC('STRDBMON OUTFILE(QTEMP/DOVEOUT)')");
             try (PreparedStatement tgt = jdbcConn.prepareStatement(sql)) {
+               // tgt.execute();
             }
-
             s.execute("CALL QSYS2.QCMDEXC('ENDDBMON')");
-            try (ResultSet rs =  s.executeQuery("SELECT QQJFLD, QQRID FROM QTEMP.DOVEOUT WHERE QQRID = 1000 limit 1")) {
-                if(rs.next()) {
+            try (ResultSet rs = s.executeQuery("SELECT QQJFLD, QQRID FROM QTEMP.DOVEOUT WHERE QQRID = 1000 limit 1")) {
+                if (rs.next()) {
                     id = rs.getBytes(1);
                 }
             }
@@ -48,24 +47,28 @@ public class DoVe extends BlockRetrievableRequest {
             } catch (Exception e) {
                 Tracer.info(e.getMessage());
             }
-            try (CallableStatement callStmt = jdbcConn.prepareCall(
-                    "call QIWS.QQQDBVE2(?,?,?,?,?)")) {
-                callStmt.setBytes(1, id);
-                callStmt.setString(2, "DOVEOUT   QTEMP     ISO-ISO..ENUBM0                           09");
-                callStmt.registerOutParameter(3, Types.INTEGER);
-                callStmt.registerOutParameter(4, Types.INTEGER);
-                callStmt.registerOutParameter(5, Types.CHAR, 72);
-                callStmt.execute();
-            }
         }
-        m_stmt = jdbcConn.createStatement();
-        m_rs = m_stmt.executeQuery("SELECT * from qtemp.QQ$DBVE_41");
-        addReplyData("metadata", getResultMetaDataForResponse());
-        addReplyData("data", super.getNextDataBlock(Integer.MAX_VALUE));
-        addReplyData("is_done", isDone());
-    }
 
-    Statement getStatement() {
-        return m_stmt;
+        try (CallableStatement callStmt = jdbcConn.prepareCall(
+                "call QIWS.QQQDBVE2(?,?,?,?,?)")) {
+            callStmt.setBytes(1, id);
+            callStmt.setString(2, "DOVEOUT   QTEMP     ISO-ISO..ENUBM0                           09");
+            callStmt.registerOutParameter(3, Types.INTEGER);
+            callStmt.registerOutParameter(4, Types.INTEGER);
+            callStmt.registerOutParameter(5, Types.CHAR, 72);
+
+            if (!callStmt.execute()) {
+                String p5 = callStmt.getString(5);
+                if(StringUtils.isNonEmpty(p5)) {
+                    p5 = p5.trim();
+                }
+                throw new RuntimeException("No result set available: "+p5);
+            }
+            callStmt.execute();
+            m_rs = callStmt.getResultSet();
+            addReplyData("metadata", getResultMetaDataForResponse());
+            addReplyData("data", super.getNextDataBlock(Integer.MAX_VALUE));
+            addReplyData("is_done", isDone());
+        }
     }
 }
