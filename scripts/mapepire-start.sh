58788bef8f5420d2ea483faaf220eae6ea0791df
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2024-08-09 10:28:39 -0500

    Update mapepire-start.sh

--- scripts/mapepire-start.sh
+++ scripts/mapepire-start.sh
@@ -1,2 +1,2 @@
 #!/QOpenSys/usr/bin/sh
-exec /QOpenSys/QIBM/ProdData/JavaVM/jdk80/64bit/bin/java -jar $(/QOpenSys/usr/bin/dirname $0)/../lib/mapepire-server.jar
+exec /QOpenSys/QIBM/ProdData/JavaVM/jdk80/64bit/bin/java -jar $(/QOpenSys/usr/bin/dirname $0)/../lib/mapepire.jar
