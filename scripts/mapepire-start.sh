8c3c219a733fe6e375511273d815b576376bb796
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2024-08-08 21:43:56 -0500

    version 2.0.1

--- scripts/mapepire-start.sh
+++ scripts/mapepire-start.sh
@@ -1,2 +1,2 @@
 #!/QOpenSys/pkgs/bin/bash
-exec /QOpenSys/QIBM/ProdData/JavaVM/jdk80/64bit/bin/java -jar /opt/mapepire-server/lib/mapepire-server.jar
+exec /QOpenSys/QIBM/ProdData/JavaVM/jdk80/64bit/bin/java -jar /opt/mapepire/lib/mapepire-server.jar
