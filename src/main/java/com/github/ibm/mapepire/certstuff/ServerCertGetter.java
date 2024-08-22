4d5001b8071c2537adb3d814c68fc221d8f161f7
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2024-08-22 09:48:01 -0500

    TLS rework: use global file for user-config cert, autoconfigure letsencrypt

--- src/main/java/com/github/ibm/mapepire/certstuff/ServerCertGetter.java
+++ src/main/java/com/github/ibm/mapepire/certstuff/ServerCertGetter.java
@@ -2,55 +2,105 @@ package com.github.ibm.mapepire.certstuff;
 
 import java.io.File;
 import java.io.IOException;
-
+import java.net.InetAddress;
+import java.util.LinkedList;
 import com.github.ibm.mapepire.Tracer;
+import com.github.theprez.jcmdutils.StringUtils;
 
 public class ServerCertGetter {
 
     private static class StoreDefaults {
-        private static String STORE_PASS = "hyrule";
-        private static String KEY_PASS = "hyrule";
-        public static String ALIAS = "wsdb";
+        private static String STORE_PASS = "mapepire";
+        private static String KEY_PASS = "mapepire";
+        public static String ALIAS = "mapepire";
 
         public static String getStorePass() {
-            String envVar = System.getenv("WSDB_STORE_PASS");
-            if (envVar != null)
+            String envVar = System.getenv("MAPEPIRE_STORE_PASS");
+            if (StringUtils.isNonEmpty(envVar))
                 return envVar;
             else
                 return StoreDefaults.STORE_PASS;
         }
 
         public static String getKeyPass() {
-            String envVar = System.getenv("WSDB_KEY_PASS");
-            if (envVar != null)
+            String envVar = System.getenv("MAPEPIRE_KEY_PASS");
+            if (StringUtils.isNonEmpty(envVar))
                 return envVar;
             else
                 return StoreDefaults.KEY_PASS;
         }
+
+        public static String getAlias() {
+            String envVar = System.getenv("MAPEPIRE_ALIAS");
+            if (StringUtils.isNonEmpty(envVar))
+                return envVar;
+            else
+                return StoreDefaults.ALIAS;
+        }
     }
 
-    private final File m_userCertFile; 
+    private final ServerCertLetsEncrypt m_letsEncrypt;
     private final File m_defaultCertFile;
+    private final File m_userCertFile;
 
     public ServerCertGetter() {
         final File userHomeDir = new File(System.getProperty("user.home"));
-        final File dotDir = new File(userHomeDir, ".wsdb");
+        final File dotDir = new File(userHomeDir, ".mapepire");
         dotDir.mkdirs();
-        m_userCertFile = new File(dotDir, "user.jks");
+        m_letsEncrypt = ServerCertLetsEncrypt.get(findLetsEncryptCertDir());
+        m_userCertFile = new File ("/QOpenSys/etc/mapepire/cert/server.jks");
         m_defaultCertFile = new File(dotDir, "default.jks");
     }
 
+    private File findLetsEncryptCertDir() {
+        File liveDir = new File("/etc/letsencrypt/live");
+        if (!liveDir.isDirectory()) {
+            return null;
+        }
+        LinkedList<File> candidates = new LinkedList<File>();
+        for (File ls : liveDir.listFiles()) {
+            if (ls.isDirectory()) {
+                candidates.add(ls);
+            }
+        }
+        if (candidates.isEmpty()) {
+            return null;
+        }
+        if (1 == candidates.size()) {
+            return candidates.removeFirst();
+        }
+
+        try {
+            String myHostName = InetAddress.getLocalHost().getCanonicalHostName().toLowerCase();
+            Tracer.info("------we think our hostname is "+myHostName);
+            for (File candidate : candidates) {
+                if (candidate.getName().equalsIgnoreCase(myHostName)) {
+                    return candidate;
+                }
+            }
+
+        } catch (IOException e) {
+        }
+        return candidates.removeLast();
+    }
+
     public ServerCertInfo get() throws IOException, InterruptedException {
-        if(m_userCertFile.isFile()) {
-            Tracer.info("Using user-defined certificate file "+m_userCertFile.getAbsolutePath());
-            return new ServerCertInfo(m_userCertFile, StoreDefaults.getStorePass(), StoreDefaults.getKeyPass(), StoreDefaults.ALIAS);
-        } else if(m_defaultCertFile.isFile()) {
-            Tracer.info("Reusing previously-generated default certificate in "+m_defaultCertFile.getAbsolutePath());
-            return new ServerCertInfo(m_defaultCertFile, StoreDefaults.getStorePass(), StoreDefaults.getKeyPass(), StoreDefaults.ALIAS);
-        } else  {
+        if (m_userCertFile.isFile()) {
+            Tracer.info("Reusing user-defined server certificate in " + m_defaultCertFile.getAbsolutePath());
+            return new ServerCertJKS(m_defaultCertFile, StoreDefaults.getStorePass(), StoreDefaults.getKeyPass(),
+                    StoreDefaults.getAlias());
+        } else if (null != m_letsEncrypt) {
+            Tracer.info("Using LetsEncrypt certificate file " + m_letsEncrypt.getKeyStoreFile().getAbsolutePath());
+            return m_letsEncrypt;
+        } else if (m_defaultCertFile.isFile()) {
+            Tracer.info("Reusing previously-generated default certificate in " + m_defaultCertFile.getAbsolutePath());
+            return new ServerCertJKS(m_defaultCertFile, StoreDefaults.getStorePass(), StoreDefaults.getKeyPass(),
+                    StoreDefaults.getAlias());
+        } else {
             Tracer.warn("Generating self-signed certificate");
             final SelfSignedCertGenerator gen = new SelfSignedCertGenerator();
-            return gen.generate(StoreDefaults.getStorePass(), StoreDefaults.getKeyPass(), m_defaultCertFile, StoreDefaults.ALIAS);
+            return gen.generate(StoreDefaults.getStorePass(), StoreDefaults.getKeyPass(), m_defaultCertFile,
+                    StoreDefaults.getAlias());
         }
     }
 }
--- src/main/java/com/github/ibm/mapepire/certstuff/ServerCertInfo.java
+++ src/main/java/com/github/ibm/mapepire/certstuff/ServerCertInfo.java
@@ -1,7 +1,6 @@
 package com.github.ibm.mapepire.certstuff;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.security.KeyStore;
@@ -9,44 +8,15 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 
-public class ServerCertInfo {
+public interface ServerCertInfo {
 
-    private final String m_storePass;
-    private final File m_storePath;
-    private KeyStore m_keystore = null;
-    private final String m_keyPass;
-    private final String m_alias;
+    public KeyStore getKeyStore() throws KeyStoreException, NoSuchAlgorithmException, FileNotFoundException, IOException, CertificateException, InterruptedException;
 
-    public ServerCertInfo(final File _keyStore, final String _storePassword, final String _keyPassword, final String _alias) {
-        m_storePath = _keyStore;
-        m_storePass = _storePassword;
-        m_keyPass = _keyPassword;
-        m_alias = _alias;
-    }
+    public String getAlias();
 
-    public KeyStore getKeyStore() throws KeyStoreException, NoSuchAlgorithmException,
-            FileNotFoundException, IOException, CertificateException {
-        if (null != m_keystore) {
-            return m_keystore;
-        }
-        final KeyStore ks = KeyStore.getInstance("JKS");
-        ks.load(new FileInputStream(m_storePath), m_storePass.toCharArray());
-        return m_keystore = ks;
-    }
+    public String getStorePass() ;
 
-    public String getAlias() {
-        return m_alias;
-    }
+    public String getKeyPass();
 
-    public String getStorePass() {
-        return m_storePass;
-    }
-
-    public String getKeyPass() {
-        return m_keyPass;
-    }
-
-    public File getKeyStoreFile() {
-        return m_storePath;
-    }
+    public File getKeyStoreFile();
 }
