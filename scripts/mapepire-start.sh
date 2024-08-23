091903b5daf07829bf02f709eabc6c76c2fbb9a5
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2024-08-22 22:21:01 -0500

    Set GC heap options

--- scripts/mapepire-start.sh
+++ scripts/mapepire-start.sh
@@ -1,2 +1,2 @@
 #!/QOpenSys/usr/bin/sh
-exec /QOpenSys/QIBM/ProdData/JavaVM/jdk80/64bit/bin/java -jar $(/QOpenSys/usr/bin/dirname $0)/../lib/mapepire/mapepire-server.jar
+exec /QOpenSys/QIBM/ProdData/JavaVM/jdk80/64bit/bin/java -Xsoftmx1G -Xmx16g -Xms256M -jar $(/QOpenSys/usr/bin/dirname $0)/../lib/mapepire/mapepire-server.jar
