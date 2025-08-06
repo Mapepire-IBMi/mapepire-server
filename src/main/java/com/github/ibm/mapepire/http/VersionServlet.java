2fa890d6cad899ffeb4ecc4c909f8568691592ad
Irfan Sharif <IrfanSharif@ibm.com>
2025-08-06 14:01:51 -0400

    Create abstract servlet logic
    Signed-off-by: Irfan Sharif <IrfanSharif@ibm.com>

--- src/main/java/com/github/ibm/mapepire/http/VersionServlet.java
+++ src/main/java/com/github/ibm/mapepire/http/VersionServlet.java
@@ -1,27 +1,19 @@
 package com.github.ibm.mapepire.http;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.github.ibm.mapepire.Version;
-import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class VersionServlet extends HttpServlet {
+public class VersionServlet extends BaseJsonServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         Map<String, String> response = new HashMap<>();
         response.put("version", Version.s_version);
-        Gson gson = new Gson();
-        String jsonResp = gson.toJson(response);
-
-        resp.setContentType("application/json");
-        resp.setCharacterEncoding("UTF-8");
-        resp.setStatus(HttpServletResponse.SC_OK);
         
-        resp.getWriter().write(jsonResp);
+        writeJsonResponse(resp, response);
     }
 }
