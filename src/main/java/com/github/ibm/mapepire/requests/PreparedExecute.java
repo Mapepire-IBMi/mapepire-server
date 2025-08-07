953eed795209c32c16946beefaf155c5d1a5c0b6
Jonathan Zak <jonnyzak32@gmail.com>
2025-08-07 12:27:07 -0400

    add support for all types

--- src/main/java/com/github/ibm/mapepire/requests/PreparedExecute.java
+++ src/main/java/com/github/ibm/mapepire/requests/PreparedExecute.java
@@ -1,11 +1,20 @@
 package com.github.ibm.mapepire.requests;
 
+import java.io.ByteArrayInputStream;
+import java.io.StringReader;
+import java.math.BigDecimal;
 import java.sql.CallableStatement;
 import java.sql.ParameterMetaData;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+
+
 import java.sql.Types;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.LinkedList;
 
 import com.github.ibm.mapepire.DataStreamProcessor;
@@ -25,6 +34,7 @@ public class PreparedExecute extends BlockRetrievableRequest {
     @Override
     protected void go() throws Exception {
         JsonArray parms = super.getRequestField("parameters").getAsJsonArray();
+        JsonArray columnTypes = super.getRequestField("columnTypes").getAsJsonArray();
         boolean isBatch = !parms.isEmpty() && parms.get(0).isJsonArray();
         boolean hasResultSet = false;
         long batchUpdateCount = 0;
@@ -34,7 +44,7 @@ public class PreparedExecute extends BlockRetrievableRequest {
             JsonArray arr = parms.getAsJsonArray();
             int batch_ops_added = 0;
             for (int i = 0; i < arr.size(); i++) {
-                addJsonArrayParameters(stmt, arr.get(i).getAsJsonArray());
+                addJsonArrayParameters(stmt, arr.get(i).getAsJsonArray(), columnTypes);
                 m_prev.getStatement().addBatch();
             }
             batch_ops_added += arr.size();
@@ -43,7 +53,7 @@ public class PreparedExecute extends BlockRetrievableRequest {
             batchUpdateCount = Arrays.stream(updateCount).sum();
         } else{
             if (parms != null) {
-                addJsonArrayParameters(stmt, parms.getAsJsonArray());
+                addJsonArrayParameters(stmt, parms.getAsJsonArray(), columnTypes);
             }
             hasResultSet = stmt.execute();
         }
@@ -66,9 +76,87 @@ public class PreparedExecute extends BlockRetrievableRequest {
         }
     }
 
-    private void addJsonArrayParameters(PreparedStatement stmt, JsonArray arr) throws SQLException {
-        for (int i = 1; i <= arr.size(); ++i) {
-            JsonElement element = arr.get(-1 + i);
+    private void addParameter(int i, JsonElement parameter, ColumnType columnType, PreparedStatement stmt) throws SQLException {
+        String stringValue = parameter.getAsString();
+
+        switch (columnType) {
+            case BLOB: {
+                byte[] decodedBytes = Base64.getDecoder().decode(stringValue);
+                ByteArrayInputStream blob = new ByteArrayInputStream(decodedBytes);
+                stmt.setBlob(i, blob);
+                break;
+            }
+            case CLOB:
+                stmt.setClob(i, new StringReader(stringValue));
+                break;
+            case INTEGER:
+                stmt.setInt(i, Integer.parseInt(stringValue));
+                break;
+            case BIGINT:
+                stmt.setLong(i, Long.parseLong(stringValue));
+                break;
+            case SMALLINT:
+                stmt.setShort(i, Short.parseShort(stringValue));
+                break;
+            case DOUBLE:
+                stmt.setDouble(i, Double.parseDouble(stringValue));
+                break;
+            case FLOAT:
+                stmt.setFloat(i, Float.parseFloat(stringValue));
+                break;
+            case DECIMAL:
+                stmt.setBigDecimal(i, new BigDecimal(stringValue));
+                break;
+            case DATE:
+                stmt.setDate(i, Date.valueOf(stringValue)); // expects ISO format: yyyy-[m]m-[d]d
+                break;
+            case TIME:
+                stmt.setTime(i, Time.valueOf(stringValue)); // expects format: hh:mm:ss
+                break;
+            case TIMESTAMP:
+                stmt.setTimestamp(i, Timestamp.valueOf(stringValue)); // expects format: yyyy-[m]m-[d]d hh:mm:ss[.f...]
+                break;
+            case BOOLEAN:
+                stmt.setBoolean(i, Boolean.parseBoolean(stringValue));
+                break;
+            default:
+                stmt.setString(i, stringValue); // Fallback
+        }
+    }
+
+
+    public enum ColumnType {
+        CHAR,
+        VARCHAR,
+        INTEGER,
+        FLOAT,
+        DECIMAL,
+        DATE,
+        TIME,
+        TIMESTAMP,
+        CLOB,
+        BLOB,
+        BOOLEAN,
+        SMALLINT,
+        BIGINT,
+        DOUBLE
+    }
+
+    private ColumnType getColumnType(int i, JsonArray columnTypes){
+        ColumnType columnType;
+        try {
+            columnType = ColumnType.valueOf(columnTypes.get(-1 + i).getAsString());
+        } catch (IllegalArgumentException e){
+            columnType = ColumnType.VARCHAR;
+        }
+        return columnType;
+    }
+
+    private void addJsonArrayParameters(PreparedStatement stmt, JsonArray parameters, JsonArray columnTypes) throws SQLException {
+        for (int i = 1; i <= parameters.size(); ++i) {
+            JsonElement element = parameters.get(-1 + i);
+            ColumnType columnType = getColumnType(-1 + i, columnTypes);
+
             if (element.isJsonNull()) {
                 stmt.setNull(i, Types.NULL);
             } else {
@@ -78,9 +166,9 @@ public class PreparedExecute extends BlockRetrievableRequest {
                 } else if (stmt instanceof CallableStatement
                         && ParameterMetaData.parameterModeInOut == stmt.getParameterMetaData().getParameterMode(i)) {
                     ((CallableStatement) stmt).registerOutParameter(i, stmt.getParameterMetaData().getParameterType(i));
-                    stmt.setString(i, element.getAsString());
+                    addParameter(i, element, columnType, stmt);
                 } else {
-                    stmt.setString(i, element.getAsString());
+                    addParameter(i, element, columnType, stmt);
                 }
             }
         }
