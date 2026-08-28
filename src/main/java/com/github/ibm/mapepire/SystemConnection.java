7263993f78e3eaab7289348f9d9736b5ba195d0d
Jesse Gorzinski <jgorzins@us.ibm.com>
2026-08-28 14:05:48 -0400

    Update SystemConnection.java

--- src/main/java/com/github/ibm/mapepire/SystemConnection.java
+++ src/main/java/com/github/ibm/mapepire/SystemConnection.java
@@ -2,6 +2,7 @@ package com.github.ibm.mapepire;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
