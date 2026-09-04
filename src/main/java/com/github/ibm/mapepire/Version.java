b2fa8b9169b3b1a1732c267a085b03955f3f7124
Jesse Gorzinski <jgorzins@us.ibm.com>
2026-09-04 13:09:26 -0400

    Delete Version.java

--- src/main/java/com/github/ibm/mapepire/Version.java
+++
@@ -1,5 +0,0 @@
-package com.github.ibm.mapepire;
-public class Version {
-      static public final String s_compileDateTime = "2026-07-28 14:46:43 (GMT)";
-      static public final String s_version = "2.3.5";
-}
